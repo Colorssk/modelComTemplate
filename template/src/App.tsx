5e2944399c865388cd35af5f93dbe4df3e680437
ming.yan <ming.yan@sumscope.com>
2022-06-07 11:32:49 +0800

    initialize component template

---
+++ template/src/App.tsx
@@ -0,0 +1,24 @@
+import { LText } from "./components";
+import "./App.css";
+import { useContext } from "react";
+import { ThemeContext } from './theme/themeProvider'
+
+function App() {
+  const { currentTheme, setCurrentTheme } = useContext(ThemeContext);
+  const changeTheme = () => {
+    // temporary
+    if(currentTheme === 'light'){
+      setCurrentTheme('dark')
+    } else {
+      setCurrentTheme('light')
+    }
+  }
+  return (
+    <div className="App">
+      <div onClick={changeTheme}>click</div>
+      <LText></LText>
+    </div>
+  );
+}
+
+export default App;
